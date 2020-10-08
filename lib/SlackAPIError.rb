2c9d971b7ad9fb0163c04264ebed6ca60dcc9126
Stacy <lundquist.stacy@gmail.com>
2020-10-07 18:53:00 -0700

    update tests require_relative

--- lib/SlackAPIError.rb
+++ lib/SlackAPIError.rb
@@ -1,3 +1,24 @@
-class SlackError < Exception
+require 'httparty'
 
-end
+class SlackError < Exception; end
+
+
+# def get_location(search_term)
+#   query = {
+#       q: search_term,
+#       key: KEY,
+#       format: "json",
+#   }
+#   response = HTTParty.get(BASE_URL, query: query)
+#
+#   unless response.code == 200
+#     raise SearchError, "Cannot find #{search_term}"
+#   end
+#
+#   return {
+#       search_term => {
+#           lat: response.first["lat"],
+#           lon: response.first["lon"],
+#       },
+#   }
+# end
